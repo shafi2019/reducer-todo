e574a503386f738463cde60e49b76a110bc2165a
Shafi Masoumi <masoumi2017@icloud.com>
2019-11-19 13:56:14 -0800

    todo card created

--- src/components/CardMap.js
+++ src/components/CardMap.js
@@ -1,10 +1,35 @@
 import React, {useState} from "react";
 
-const CardMap = () => {
+const CardMap = ({ bMap, dispatch}) => {
+    // console.log(bMap);
+
+    const toggleCompleted = () => {
+        dispatch ({
+            type: "TOGGLE_EDIT",
+            payload: bMap.id
+        });
+    };
+
+    const clearCompletedTodo = () => {
+        dispatch ({
+            type: "DONE_TOGGLE",
+            payload: bMap.id
+        });
+    }
 
     return (
         <div className="Card">
-
+            <div className="cTop">
+                <div 
+                    className={`bMap${bMap.completed ? ' completed' : ''}`}
+                    onClick={toggleCompleted}
+                >
+                    <h1>{bMap.item}</h1>
+                </div>
+            </div>
+            <div className="cBottom">
+                <button className="bCard" onClick={clearCompletedTodo}>Clear Completed</button>
+            </div>
         </div>
     )
 }
